4d6603c4291259acd6f69bedc2bd811d129470d7
MichaelAlexanderJ <Michaelalex2705@gmail.com>
2022-12-02 11:50:09 +0700

    query dikit masih harus revisi

--- controller/feature_graphbar.js
+++ controller/feature_graphbar.js
@@ -1,5 +1,5 @@
 import express from "express";
-
+import { countCharacter } from "../query/graphbarQuery.js";
 const router = express.Router();
 
 router.get('/',(req,res)=>{
--- query/graphbarQuery.js
+++ query/graphbarQuery.js
@@ -1,10 +1,10 @@
 
 import mysql from 'mysql';
 
-export const searchName = async (name) => {
+export const countCharacter = async (name) => {
     const conn = await dbConnect();
     return new Promise((resolve, reject) => {
-        const sql = `SELECT Target FROM book_gabungan WHERE Source LIKE '%${name}%'`;
+        const sql = `SELECT Source FROM book_gabungan WHERE Source LIKE '%${name}%'`;
         conn.query(sql, name, (err, result) => {
             if (err) {
                 reject(err);
