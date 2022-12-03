cdea6817ece6c8da7595d67b989832a3f7311d23
MichaelAlexanderJ <Michaelalex2705@gmail.com>
2022-12-03 20:16:27 +0700

    coba query graphbar

--- controller/feature_graphbar.js
+++ controller/feature_graphbar.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { countCharacter } from "../query/graphbarQuery.js";
+import { countCharacter1 } from "../query/graphbarQuery.js";
 const router = express.Router();
 
 router.get('/',(req,res)=>{
     res.render('feature_graphbar');
+    countCharacter1();
+
 });
 
 export {router as graphbar};
--- query/graphbarQuery.js
+++ query/graphbarQuery.js
@@ -1,11 +1,11 @@
 
 import mysql from 'mysql';
 
-export const countCharacter = async (name) => {
+export const countCharacter1 = async () => {
     const conn = await dbConnect();
     return new Promise((resolve, reject) => {
-        const sql = `SELECT Source FROM book_gabungan WHERE Source LIKE '%${name}%'`;
-        conn.query(sql, name, (err, result) => {
+        const sql = `SELECT distinct Source,Count(Source) AS jumlahNama FROM book_gabungan WHERE book LIKE 'book1' ORDER BY Count(Source) DESC limit 10`;
+        conn.query(sql,(err, result) => {
             if (err) {
                 reject(err);
             } else {
