cf25767e456059685991a383fa02923775519e62
MichaelAlexanderJ <Michaelalex2705@gmail.com>
2022-12-11 00:10:29 +0700

    ngerjain front end search :))

--- controller/feature_search.js
+++ controller/feature_search.js
@@ -3,8 +3,28 @@ import { dbConnect } from '../controller/database.js';
 
 const router = express.Router();
 
+const getNama = conn => {
+    return new Promise((resolve,reject) => {
+        conn.query(`SELECT `,(err,result) => {
+            if(err){
+                reject(err);
+            }else{
+                resolve(result);
+            }
+        })
+    })
+}
+
 router.get('/',(req,res)=>{
     res.render('feature_search');
 });
 
+router.post('/searchNama',express.urlencoded(),async(req,res)=>{
+
+    const conn = await dbConnect();
+    
+    conn.release();
+
+    console.log(xValues);
+});
 export {router as search};
