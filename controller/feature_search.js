f82fae257bab89f002362d44b6550cae7b888065
MichaelAlexanderJ <Michaelalex2705@gmail.com>
2022-12-13 01:53:45 +0700

    coba coba masi ngaco

--- controller/feature_search.js
+++ controller/feature_search.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { text } from "express";
 import { dbConnect } from '../controller/database.js';
 
 const router = express.Router();
@@ -6,6 +6,18 @@ const router = express.Router();
 const getNama1 = (conn,nama) => {
     return new Promise((resolve,reject) => {
         conn.query(`SELECT * FROM book1 WHERE nama LIKE ?  `,(err,result) => {
+            if(err){
+                reject(err);
+            }else{
+                resolve(result);
+            }
+        })
+    })
+}
+
+const getAutoComplete1 = (conn,nama) => {
+    return new Promise((resolve,reject) => {
+        conn.query(`SELECT Source FROM book1 WHERE Source LIKE '%${nama}%' LIMIT 10 `,(err,result) => {
             if(err){
                 reject(err);
             }else{
@@ -25,6 +37,25 @@ router.post('/searchNama',express.urlencoded(),async(req,res)=>{
     const conn = await dbConnect();
     const result = await getNama1(conn,nama);
     conn.release();
+
+});
+
+router.get('/get_data',express.urlencoded(),async(req, res, next) =>{
+    const conn = await dbConnect();
+    const search_query = req.query.search_query;
+
+    // var query = `
+    // SELECT country_name FROM apps_countries 
+    // WHERE country_name LIKE '%${search_query}%' 
+    // LIMIT 10
+    // `;
+
+    // conn.query(query, function(error, data){
+    //     console.log(data);
+    //     response.json(data);
+
+    // });
+    res.json(await getAutoComplete1(conn,search_query)); 
 
 });
 
